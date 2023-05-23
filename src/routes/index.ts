9316d3dfcae9dbc89e9f17154d096579a8d66957
Vihiga Tyonum <withtvpeter@gmail.com>
2023-05-23 04:33:31 +0100

    add swagger documentation package

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,22 +6,22 @@ import Validator from '../utils/Validator';
 const router = Router();
 
 router.get(
-	'/lnd/deposits/',
+	'/lnd/deposits',
 	Validator.validateEmaliAndAmount(),
 	ValidationHandler.handleError,
 	PaymentsController.getPaymentRequest,
 );
 
 router.get(
-	'/lnd/withdrawals/',
+	'/lnd/withdrawals',
 	Validator.validateEmaliAndAmount(),
 	ValidationHandler.handleError,
 	PaymentsController.getWithdrawalRequest,
 );
 
 router.get(
-	'/lnurl/withdrawals/',
-	Validator.validateInvoice(),
+	'/lnurl/withdrawals',
+	Validator.validateInvoice,
 	ValidationHandler.handleError,
 	PaymentsController.payInvoice,
 );
