b1a733b04013833605c8a6bb75f4db62c382df32
devmasme <devmasme@gmail.com>
2023-05-31 16:20:15 +0900

    feat: set sentry

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,51 @@
+import * as Sentry from '@sentry/node';
+
 import express, { Express, Request, Response } from 'express';
-import router from './routes';
-import helmet from 'helmet';
+import { onLNDDeposit, onLNDWithdrawal } from './events/LNDEvent';
+
+import { AuthenticatedLnd } from 'lightning';
+import { HttpCodes } from './enums/HttpCodes';
+import ResponseDto from './dto/ResponseDto';
 import cors from 'cors';
 import env from './config/env';
-import logger from './utils/logger';
-import { HttpCodes } from './enums/HttpCodes';
-import initLND from './config/initLND';
-import { AuthenticatedLnd } from 'lightning';
-import initLUD from './config/initLUD';
-import { onLNDDeposit, onLNDWithdrawal } from './events/LNDEvent';
-import { onLUDFail } from './events/LUDEvent';
+import helmet from 'helmet';
 import initDB from './domains/initDB';
-import ResponseDto from './dto/ResponseDto';
+import initLND from './config/initLND';
+import initLUD from './config/initLUD';
+import logger from './utils/logger';
+import { onLUDFail } from './events/LUDEvent';
+import router from './routes';
 
 const app: Express = express();
 const port = Number(env.SERVER_PORT);
+
+Sentry.init({
+	dsn: env.SENTRY_DSN,
+	integrations: [
+		// enable HTTP calls tracing
+		new Sentry.Integrations.Http({ tracing: true }),
+		// enable Express.js middleware tracing
+		new Sentry.Integrations.Express({ app }),
+		// Automatically instrument Node.js libraries and frameworks
+		...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+	],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+
+	// Don't capture local error
+	enabled:
+		process.env.NODE_ENV === 'production' ||
+		process.env.NODE_ENV === 'development',
+});
+
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(helmet());
 app.use(cors());
@@ -48,6 +79,7 @@ app.listen(port, async () => {
 		await onLUDFail(lud);
 	} catch (error) {
 		console.error(error);
+		Sentry.captureMessage(`Server initialization error: ${error}`);
 		logger.error(error);
 	}
 });
--- src/utils/ValidationHandler.ts
+++ src/utils/ValidationHandler.ts
@@ -1,12 +1,22 @@
-import { Request, Response, NextFunction } from 'express';
-import { validationResult } from 'express-validator';
+import * as Sentry from '@sentry/node';
+
+import { NextFunction, Request, Response } from 'express';
+
 import { HttpCodes } from '../enums/HttpCodes';
 import ResponseDto from '../dto/ResponseDto';
+import { validationResult } from 'express-validator';
 
 class ValidationHandler {
 	static handleError(req: Request, res: Response, next: NextFunction) {
 		const error = validationResult(req);
 		if (!error.isEmpty()) {
+			Sentry.captureMessage(
+				`Validation error: ${error
+					.array()
+					.map(e => e.msg)
+					.join(', ')}
+				`,
+			);
 			return res
 				.status(HttpCodes.BAD_REQUEST)
 				.json(
