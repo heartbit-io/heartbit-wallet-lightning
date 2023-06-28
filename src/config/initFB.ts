e0f92acc4431ca13a99a20c629d7f646c5333325
ChrisCho-H <hyeonheum@naver.com>
2023-06-28 11:47:21 +0900

    feat: add firebase env and init config

---
+++ src/config/initFB.ts
@@ -0,0 +1,26 @@
+import admin from 'firebase-admin';
+import env from './env';
+import { initializeApp } from 'firebase/app';
+
+export default admin.initializeApp({
+	credential: admin.credential.cert({
+		projectId: env.FB_PROJECT_ID,
+		privateKey: env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+		clientEmail: env.FB_CLIENT_EMAIL,
+	}),
+	// databaseURL: `https://${env.FB_PROJECT_ID}.firebaseio.com`,
+});
+
+const firebaseConfig = {
+	apiKey: env.FB_API_KEY,
+	authDomain: env.FB_AUTH_DOMAIN,
+	projectId: env.FB_PROJECT_ID,
+	storageBucket: env.FB_STORAGE_BUCKET,
+	messagingSenderId: env.FB_MEASUREMENT_ID,
+	appId: env.FB_APP_ID,
+	measurementId: env.FB_MEASUREMENT_ID,
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export { firebase };
